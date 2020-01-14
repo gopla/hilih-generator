c7e0673dcbf9374acb5d74dca869d46f4b56df7c
Naufal Yudhistira <yudhistn@gmail.com>
2020-01-14 21:24:24 +0700

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$("#input").keyup(function(e) {
+  let s = $("#input").val();
+
+  s = s.replace(/\a/g, "i");
+  s = s.replace(/\e/g, "i");
+  s = s.replace(/\u/g, "i");
+  s = s.replace(/\o/g, "i");
+
+  $("#output").val(s);
+});
+
+$("#btn-reset").click(function(e) {
+  e.preventDefault();
+  $("#input").val("");
+  $("#output").val("");
+});
