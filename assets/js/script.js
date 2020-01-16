d2b8f2a527a9d6c9d6b58dfede0b5ae9ef31401f
Naufal Yudhistira <yudhistn@gmail.com>
2020-01-16 12:46:52 +0700

    Fixing bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
-$("#input").keyup(function(e) {
-  let s = $("#input").val();
+$(function() {
+  $("#input").keyup(function(e) {
+    let s = $("#input").val();
 
-  s = s.replace(/\a/g, "i");
-  s = s.replace(/\e/g, "i");
-  s = s.replace(/\u/g, "i");
-  s = s.replace(/\o/g, "i");
+    s = s.replace(/\a/g, "i");
+    s = s.replace(/\e/g, "i");
+    s = s.replace(/\u/g, "i");
+    s = s.replace(/\o/g, "i");
 
-  $("#output").val(s);
-});
+    $("#output").val(s);
+  });
 
-$("#btn-reset").click(function(e) {
-  e.preventDefault();
-  $("#input").val("");
-  $("#output").val("");
+  $("#btn-reset").click(function(e) {
+    e.preventDefault();
+    $("#input").val("");
+    $("#output").val("");
+  });
 });
